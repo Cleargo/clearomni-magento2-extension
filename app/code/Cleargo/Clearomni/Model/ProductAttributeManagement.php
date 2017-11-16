93fd45c0782d5142bef9181aba2e556a8bf08489
GX <guangxiangk@gmail.com>
2017-11-16 16:03:51 +0800

    update - cleaomni api

--- app/code/Cleargo/Clearomni/Model/ProductAttributeManagement.php
+++ app/code/Cleargo/Clearomni/Model/ProductAttributeManagement.php
@@ -10,26 +10,38 @@ class ProductAttributeManagement
      */
     public function getFrontendLabels($attribute_code)
     {
+        $attribute_code = explode(',', $attribute_code);
+
+        $_attribute_code = '';
+        foreach ($attribute_code as $key => $code) {
+            if( $key == 0 ) {
+                $_attribute_code .= "'". $code ."'";
+                // $_attribute_code .= $code;
+            } else {
+                $_attribute_code .= ",'". $code ."'";
+                // $_attribute_code .= ",". $code;
+            }            
+        }
+        
         $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
 
         $resource = $objectManager->get('Magento\Framework\App\ResourceConnection');
         $connection = $resource->getConnection();
 
         $select = $connection->select()
-                ->from(
-                    ['eav' => 'eav_attribute'],
-                    ['attribute_id', 'attribute_code']
-                )
-                ->join(
-                    ['eav_label' => 'eav_attribute_label'],
-                    'eav.attribute_id = eav_label.attribute_id',
-                    ['store_id', 'value']                         
-                )
-                ->where('eav.attribute_code in (?)', $attribute_code )
-                // ->where('cpe.entity_id=?', $productId )
-                ;
-
-        $data = $connection->fetchAll($select);
+            ->from(
+                ['eav' => 'eav_attribute'],
+                ['attribute_id', 'attribute_code']
+            )
+            ->join(
+                ['eav_label' => 'eav_attribute_label'],
+                'eav.attribute_id = eav_label.attribute_id',
+                ['store_id', 'value']                         
+            )
+            ->where('eav.attribute_code in (?)', $attribute_code )
+            // ->where('cpe.entity_id=?', $productId )
+            ;
+        $data = $connection->fetchAll($select);        
 
         foreach( $data as $key => $d ) {
             $data[$key]['locale'] = $this->_getStoreLocale($d['store_id']);            
